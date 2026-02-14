e7d505fb05134b1fc807c044da82a449196fcf47
demo <rahulusain11@gmail.com>
2026-02-14 14:36:10 +0530

    Updated CORS for production

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const contactRoutes = require('./routes/contact.routes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
